6fc347f10e5acfcf3c541406b570da82325ad393
HodaEbrahimMosaad <58063429+HodaEbrahimMosaad@users.noreply.github.com>
2021-01-08 02:48:47 +0200

    comment,other

--- js/cookies.js
+++ js/cookies.js
@@ -1,23 +1,17 @@
- function hasCookie(name) {
-    var user = getCookie(name);
-    if (user == "") {
-        return false;
-    } else {
+function hasCookie(name) {
+    if(getCookie(name))
         return true;
-    }
+    else return false;
 }
-
  function setCookie(cname, cvalue, exdays) {
     var d = new Date();
     d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
     var expires = "expires=" + d.toGMTString();
     document.cookie = cname + "=" + cvalue + ";" + expires;
 }
-
  function deleteCookie(cookieName) {
     document.cookie = cookieName + "=; expires=Thu, 30 Jan 1970 00:00:00 UTC; ";
 }
-
 function getCookie(cname) {
     var name = cname + "=";
     var decodedCookie = decodeURIComponent(document.cookie);
@@ -32,8 +26,6 @@ function getCookie(cname) {
         }
     }
 }
-
-
  function allCookieList() {
     var cookieArr = []
     var cookies = document.cookie.split(";");
